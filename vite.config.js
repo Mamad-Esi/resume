cf4708ce8d4cd7685701a5b9ecd8861874c4ee84
Mamad-Esi <Mamad.essi4030@example.com>
2024-12-22 05:17:27 -0800

    vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'page/about.html'),
+        contact: resolve(__dirname, 'page/contact.html'),
+        single: resolve(__dirname, 'page/single.html'),
+        work: resolve(__dirname, 'page/Work.html'),
+        works: resolve(__dirname, 'page/Works.html'),
+      }
+    }
+  }
+});
